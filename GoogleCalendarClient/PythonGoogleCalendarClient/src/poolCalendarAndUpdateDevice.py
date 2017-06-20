22a89032613f6c2747fd83b12422b97c54ba683c
BERNHARD Raphaël <raphael.bernhard@orange.fr>
2017-06-20 19:52:47 +0200

    to be tested

--- GoogleCalendarClient/PythonGoogleCalendarClient/src/poolCalendarAndUpdateDevice.py
+++ GoogleCalendarClient/PythonGoogleCalendarClient/src/poolCalendarAndUpdateDevice.py
@@ -28,6 +28,95 @@ import datetime
 _calendar_id = 'mamie.lora06@gmail.com'
 _found_calendar = None
 
+'''
+########################################"
+MamieLora interface functions
+'''
+
+import http.client
+import urllib.request
+import json
+
+_debug = False
+def activate_http_debug ():
+    global _debug
+    
+    _debug = True
+    
+    http.client.HTTPConnection.debuglevel = 1
+    http.client.HTTPSConnection.debuglevel = 1
+    
+_urllib_proxy_specification = {}    
+
+def set_http_proxy_configuration (proxy_host, proxy_port = None):
+    global _urllib_proxy_specification
+    
+    if proxy_port:
+        proxy_url = 'http://%s:%d/' % (proxy_host, proxy_port)
+    else:
+        proxy_url = 'http://%s:8080/' % proxy_host
+        
+    _urllib_proxy_specification = {'http': proxy_url, 'https': proxy_url }
+
+def updateMamieLoraDeviceStatus (device_status):
+    
+    url = 'https://lpwa.liveobjects.orange-business.com/api/v0/vendors/lora/devices/0102030405060789/commands'
+    headers = {}
+    
+    headers["Content-Type"] = "application/json;charset=UTF-8"
+    headers["Accept"] = "application/json"
+    headers["X-API-KEY"] = "6e1eefa3b6bc4fadb9158bbd2bbf587c"
+    
+    json_body_arg = {
+        "data": "02",
+        "port": "1",
+        "confirmed": "false"        
+    }
+    
+    
+    json_str = json.dumps (json_body_arg)
+    
+    request_body = json_str.encode(encoding='utf_8')
+    
+    try:
+        
+        if _debug:
+            http_debug_level_flag = 1
+        else:
+            http_debug_level_flag = 0
+        
+        http_handler = urllib.request.HTTPHandler(debuglevel=http_debug_level_flag)
+        
+        proxy_handler = urllib.request.ProxyHandler (_urllib_proxy_specification)
+        
+        opener = urllib.request.build_opener(http_handler, proxy_handler)
+
+        # install it
+        urllib.request.install_opener(opener)
+        
+        req = urllib.request.Request(url=url, headers=headers, data=request_body, method='GET')
+
+        response = urllib.request.urlopen(req, timeout=10)
+        b_data = response.read()
+        response_data = b_data.decode('utf-8')
+        
+        print (response_data)
+        
+        #Load json with request text
+        jsonData = json.loads(response_data)
+        
+        if  'token' in jsonData:
+            _keeex_current_auth_token = jsonData["token"]
+
+    except urllib.error.HTTPError as e:
+        print (e)
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+  
+    except urllib.error.URLError as e:
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)   
+
 
 def main(argv):
     # Authenticate and construct service.
